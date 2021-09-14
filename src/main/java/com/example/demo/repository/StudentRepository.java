580b22e809378ff108106e23d524267c7b75fcb7
Your Name <tuanphuc2k@gmail.com>
2021-09-14 23:05:47 +0700

    master

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -2,6 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+
 }

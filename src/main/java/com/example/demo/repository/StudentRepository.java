815866f2769d5ca7acc8c0b8a383feb778bd503f
Your Name <tuanphuc2k@gmail.com>
2021-09-14 23:15:51 +0700

    native

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -12,6 +12,22 @@ public interface StudentRepository extends JpaRepository<Student,Integer> {
     @Query("select s from Student s")
     List<Student>getAll();
 
+    @Transactional
+    @Modifying
+    @Query(value = "insert into Student(name, score) values(?1,?2)", nativeQuery = true)
+    void addStudent(String name, Double score);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update Student s set s.name = ?2, s.score =?3 where s.id = ?1",nativeQuery = true)
+    void updateStudent(Integer id, String name, Double score);
+
+    @Transactional
+    @Modifying
+    @Query(value ="delete from Student s where s.id = ?1",nativeQuery = true)
+    void deleteStudent(Integer id);
+
+
 
 
 
